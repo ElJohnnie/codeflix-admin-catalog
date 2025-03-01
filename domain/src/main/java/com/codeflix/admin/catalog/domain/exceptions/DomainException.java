0988e615e0dd79b703fd4789924b7f8e629b377e
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-03-01 20:29:38 -0300

    fix: refine stack errors

--- domain/src/main/java/com/codeflix/admin/catalog/domain/exceptions/DomainException.java
+++ domain/src/main/java/com/codeflix/admin/catalog/domain/exceptions/DomainException.java
@@ -3,17 +3,21 @@ package com.codeflix.admin.catalog.domain.exceptions;
 import java.util.List;
 import com.codeflix.admin.catalog.domain.validation.Error;
 
-public class DomainException extends RuntimeException {
+public class DomainException extends NoStacktraceException {
 
     protected final List<Error> errors;
 
-    protected DomainException(final List<Error> anErrors) {
-        super("", null, true, false);
+    protected DomainException(final String aMessage, final List<Error> anErrors) {
+        super(aMessage);
         this.errors = anErrors;
     }
 
+    public static DomainException with( final Error anErrors) {
+        return new DomainException(anErrors.message(), List.of(anErrors));
+    }
+
     public static DomainException with(final List<Error> anErrors) {
-        return new DomainException(anErrors);
+        return new DomainException("", anErrors);
     }
 
     public List<Error> getErrors() {
