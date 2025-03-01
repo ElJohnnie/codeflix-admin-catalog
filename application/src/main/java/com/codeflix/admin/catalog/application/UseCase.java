5733798f022e332522c439f34b6998e605cc8df3
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-03-01 16:18:01 -0300

    feat: init ddd boilerplate

--- application/src/main/java/com/codeflix/admin/catalog/application/UseCase.java
+++ application/src/main/java/com/codeflix/admin/catalog/application/UseCase.java
@@ -1,6 +1,6 @@
 package com.codeflix.admin.catalog.application;
 
-import com.codeflix.admin.catalog.domain.Category;
+import com.codeflix.admin.catalog.domain.category.Category;
 
 public class UseCase {
     public Category execute() {
