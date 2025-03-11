4a1b240bec0d07638e1f14256a05dd5f27479b6d
Jonatha <jonatha.follmer@telefonicati.onmicrosoft.com>
2025-03-11 16:11:00 -0300

    perf: init notification pattern

--- application/src/main/java/com/codeflix/admin/catalog/application/category/create/DefaultCreateCategoryUseCase.java
+++ application/src/main/java/com/codeflix/admin/catalog/application/category/create/DefaultCreateCategoryUseCase.java
@@ -2,6 +2,7 @@ package com.codeflix.admin.catalog.application.category.create;
 
 import com.codeflix.admin.catalog.domain.category.Category;
 import com.codeflix.admin.catalog.domain.category.CategoryGateway;
+import com.codeflix.admin.catalog.domain.validation.handler.Notification;
 import com.codeflix.admin.catalog.domain.validation.handler.ThrowsValidationHandler;
 
 import java.util.Objects;
@@ -19,10 +20,15 @@ public class DefaultCreateCategoryUseCase extends CreateCategoryUseCase {
         final var aName = aCommand.name();
         final var aDescription = aCommand.description();
         final var isActive = aCommand.isActive();
+        final var notification = Notification.create();
 
         final var aCategory = Category.newCategory(aName, aDescription, isActive);
 
-        aCategory.validate(new ThrowsValidationHandler());
+        aCategory.validate(notification);
+
+        if (notification.hasError()) {
+            //
+        }
 
         return CreateCategoryOutput.from(this.categoryGateway.create(aCategory));
     }
