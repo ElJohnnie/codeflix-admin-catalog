61604c3b2789a9916d6d139c41c427202235bb9e
ElJohnnie <johnfollmer1995@gmail.com>
2025-07-11 18:16:07 +0000

    create test data builder to category entity

--- src/category/infra/db/__tests__/category-in-memory.repository.spec.ts
+++ src/category/infra/db/__tests__/category-in-memory.repository.spec.ts
@@ -19,10 +19,71 @@ describe('CategoryInMemoryRepository', () => {
 
 
   it('should return all items if filter is empty', async () => {
-    const items = [makeCategory({ name: 'A' }), makeCategory({ name: 'B' })];
+    const items = [Category.fake().theCategories(3).build()];
     repository.items = items;
     const filtered = await repository["applyFilter"](items, '');
     expect(filtered).toEqual(items);
+  });
+
+    it('should no filter items when filter object is null', async () => {
+    const items = [Category.fake().aCategory().build()];
+    const filterSpy = jest.spyOn(items, 'filter' as any);
+
+    const itemsFiltered = await repository['applyFilter'](items, null);
+    expect(filterSpy).not.toHaveBeenCalled();
+    expect(itemsFiltered).toStrictEqual(items);
+  });
+
+    it('should filter items using filter parameter', async () => {
+    const items = [
+      Category.fake().aCategory().withName('test').build(),
+      Category.fake().aCategory().withName('TEST').build(),
+      Category.fake().aCategory().withName('fake').build(),
+    ];
+    const filterSpy = jest.spyOn(items, 'filter' as any);
+
+    const itemsFiltered = await repository['applyFilter'](items, 'TEST');
+    expect(filterSpy).toHaveBeenCalledTimes(1);
+    expect(itemsFiltered).toStrictEqual([items[0], items[1]]);
+  });
+
+    it('should sort by created_at when sort param is null', async () => {
+    const created_at = new Date();
+
+    const items = [
+      Category.fake()
+        .aCategory()
+        .withName('test')
+        .withCreatedAt(created_at)
+        .build(),
+      Category.fake()
+        .aCategory()
+        .withName('TEST')
+        .withCreatedAt(new Date(created_at.getTime() + 100))
+        .build(),
+      Category.fake()
+        .aCategory()
+        .withName('fake')
+        .withCreatedAt(new Date(created_at.getTime() + 200))
+        .build(),
+    ];
+
+    const itemsSorted = await repository['applySort'](items, null, null);
+    expect(itemsSorted).toStrictEqual([items[2], items[1], items[0]]);
+  });
+
+    it('should sort by name', async () => {
+    const items = [
+      Category.fake().aCategory().withName('c').build(),
+      Category.fake().aCategory().withName('b').build(),
+      Category.fake().aCategory().withName('a').build(),
+    ];
+
+    let itemsSorted = await repository['applySort'](items, 'name', 'asc');
+    expect(itemsSorted).toStrictEqual([items[2], items[1], items[0]]);
+
+    itemsSorted = await repository['applySort'](items, 'name', 'desc');
+    expect(itemsSorted).toStrictEqual([items[0], items[1], items[2]]);
   });
 
   it('should sort by name ascending', () => {
