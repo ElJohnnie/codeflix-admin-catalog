66e89d007c4287bec1b04583a47a6174b3d3e14b
ElJohnnie <johnfollmer1995@gmail.com>
2025-07-14 22:46:19 +0000

    fix category model and improve tests

--- src/category/infra/db/sequelize/category.model.ts
+++ src/category/infra/db/sequelize/category.model.ts
@@ -1,29 +1,21 @@
 import { Column, DataType, Model, PrimaryKey, Table } from "sequelize-typescript";
 
 
-@Table({ tableName: "categories",
-  timestamps: false, })
+@Table({ tableName: 'categories', timestamps: false })
 export class CategoryModel extends Model {
   @PrimaryKey
-  @Column({
-    type: DataType.UUID,
-  })
+  @Column({ type: DataType.UUID })
   declare category_id: string;
 
-  @Column({
-    allowNull: false,
-    type: DataType.STRING(255),
-  })
+  @Column({ allowNull: false, type: DataType.STRING(255) })
   declare name: string;
 
-  @Column({
-    type: DataType.TEXT,
-  })
+  @Column({ allowNull: true, type: DataType.TEXT })
   declare description: string | null;
 
-  @Column({
-    type: DataType.BOOLEAN,
-  })
+  @Column({ allowNull: false, type: DataType.BOOLEAN })
   declare is_active: boolean;
+
+  @Column({ allowNull: false, type: DataType.DATE(3) })
   declare created_at: Date;
 }
