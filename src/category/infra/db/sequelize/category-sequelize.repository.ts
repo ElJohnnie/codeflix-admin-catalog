1f3a57a425d5da203f4c200740ae5f3a5f2d7833
ElJohnnie <johnfollmer1995@gmail.com>
2025-07-15 21:33:14 +0000

    if dont have finded a category, returns null

--- src/category/infra/db/sequelize/category-sequelize.repository.ts
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -63,7 +63,7 @@ export class CategorySequelizeRepository implements ICategoryRepository {
   async findById(entity_id: Uuid): Promise<Category | null> {
     const model = await this._get(entity_id.id);
     if (!model) {
-      throw new NotFoundError(entity_id, this.getEntity());
+      return null;
     }
     return new Category({
       category_id: new Uuid(model.category_id),
