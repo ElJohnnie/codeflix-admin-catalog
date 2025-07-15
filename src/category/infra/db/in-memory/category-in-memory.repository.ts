71a1dda15dd81b1b78ba25f5ec26c686eab37d60
ElJohnnie <johnfollmer1995@gmail.com>
2025-07-15 19:34:35 +0000

    create category repository

--- src/category/infra/db/in-memory/category-in-memory.repository.ts
+++ src/category/infra/db/in-memory/category-in-memory.repository.ts
@@ -2,8 +2,9 @@ import { SortDirection } from "../../../../@shared/domain/repository/search-para
 import { Uuid } from "../../../../@shared/domain/value-objects/uuid.vo";
 import { InMemorySearchableRepository } from "../../../../@shared/infra/db/in-memory/in-memory.repository";
 import { Category } from "../../../domain/category.entity";
+import { ICategoryRepository } from "../../../domain/category.repository";
 
-export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> {
+export class CategoryInMemoryRepository extends InMemorySearchableRepository<Category, Uuid> implements ICategoryRepository {
   sortableFields: string[] = ['name', 'created_at'];
   protected async applyFilter(items: Category[], filter: string): Promise<Category[]> {
      if (!filter) {
@@ -20,7 +21,12 @@ export class CategoryInMemoryRepository extends InMemorySearchableRepository<Cat
     throw new Error("Method not implemented.");
   }
   
-  protected applySort(items: Category[], sort: string | null, sort_dir: SortDirection | null) {
-    return sort ? super.applySort(items, sort, sort_dir) : super.applySort(items, 'created_at', 'desc');
+  protected applySort(
+    items: Category[], 
+    sort: string | null, 
+    sort_dir: SortDirection | null) {
+    return sort ? 
+      super.applySort(items, sort, sort_dir) 
+      : super.applySort(items, 'created_at', 'desc');
   }
 }
---
+++ src/category/infra/db/sequelize/category-sequelize.repository.ts
@@ -0,0 +1,124 @@
+import { Op } from "sequelize";
+import { NotFoundError } from "../../../../@shared/domain/errors/not-found.error";
+import { Uuid } from "../../../../@shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../domain/category.entity";
+import { CategorySearchParams, CategorySearchResult, ICategoryRepository } from "../../../domain/category.repository";
+import { CategoryModel } from "./category.model";
+
+export class CategorySequelizeRepository implements ICategoryRepository {
+
+  constructor(private categoryModel: typeof CategoryModel) {}
+  sortableFields: string[] = ['name', 'created_at'];
+
+  async insert(entity: Category): Promise<void> {
+    await this.categoryModel.create(
+      {
+        category_id: entity.category_id.id,
+        name: entity.name,
+        description: entity.description,
+        is_active: entity.is_active,
+        created_at: entity.created_at,
+      }
+    );
+  }
+  async bulkInsert(entities: Category[]): Promise<void> {
+    await this.categoryModel.bulkCreate(
+      entities.map(entity => ({
+        category_id: entity.category_id.id,
+        name: entity.name,
+        description: entity.description,
+        is_active: entity.is_active,
+        created_at: entity.created_at,
+      }))
+    );
+  }
+
+  async update(entity: Category): Promise<void> {
+    const model = await this._get(entity.category_id.id);
+    if (!model) {
+      throw new NotFoundError(entity.category_id.id, this.getEntity());
+    }
+    await this.categoryModel.update({
+      category_id: entity.category_id.id,
+      name: entity.name,
+      description: entity.description,
+      is_active: entity.is_active,
+      created_at: entity.created_at,
+    }, {
+      where: { category_id: entity.category_id.id }
+    });
+  }
+
+  async delete(entity_id: Uuid): Promise<void> {
+    const model = await this._get(entity_id.id);
+    if (!model) {
+      throw new NotFoundError(entity_id, this.getEntity());
+    }
+    this.categoryModel.destroy({
+      where: { category_id: entity_id.id }
+    });
+  }
+
+  async findById(entity_id: Uuid): Promise<Category | null> {
+    const model = await this._get(entity_id.id);
+    return new Category({
+      category_id: new Uuid(model.category_id),
+      name: model.name,
+      description: model.description,
+      is_active: model.is_active,
+      created_at: model.created_at,
+    })
+  }
+
+  async findAll(): Promise<Category[]> {
+    const models = await this.categoryModel.findAll();
+    return models.map(model => new Category({
+      category_id: new Uuid(model.category_id),
+      name: model.name,
+      description: model.description,
+      is_active: model.is_active,
+      created_at: model.created_at,
+    }));
+  }
+  getEntity(): new (...args: any[]) => Category {
+    return Category
+  }
+
+async search(props: CategorySearchParams): Promise<CategorySearchResult> {
+  const offset = (props.page - 1) * props.per_page;
+  const limit = props.per_page;
+  const { rows: models, count} = await this.categoryModel.findAndCountAll({
+    ...(props.filter && {
+    where: {
+      name: {
+        [Op.like]: `%${props.filter}%`
+      }
+    }
+  }),
+  ...(props.sort && this.sortableFields.includes(props.sort) ? {
+    order: [[props.sort, props.sort_dir]]
+  } : {
+    order: [['created_at', 'desc']]
+  }),
+  offset,
+  limit
+  })
+  return new CategorySearchResult({
+    items: models.map(model => new Category({
+      category_id: new Uuid(model.category_id),
+      name: model.name,
+      description: model.description,
+      is_active: model.is_active,
+      created_at: model.created_at,
+    })),
+    total: count,
+    current_page: props.page,
+    per_page: props.per_page,
+  })
+}
+
+  private async _get(id: string) {
+    return await this.categoryModel.findByPk(id);
+  }
+
+}
