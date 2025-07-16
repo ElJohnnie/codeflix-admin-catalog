5eab396a71d6f3028a830c6f8bfaef0e8f037c6c
ElJohnnie <johnfollmer1995@gmail.com>
2025-07-16 23:11:48 +0000

    created a category validate in category model mapper

---
+++ src/category/infra/db/sequelize/__tests__/category-model-mapper.int-spec.ts
@@ -0,0 +1,48 @@
+import { Sequelize } from "sequelize-typescript";
+import { EntityValidationError } from "../../../../../@shared/domain/validators/validation.error";
+import { CategoryModelMapper } from "../category-model-mapper";
+import { CategoryModel } from "../category.model";
+
+describe("CategoryModelMapper Integration Tests", () => {
+  let sequelize: Sequelize;
+
+  beforeAll(async () => {
+    sequelize = new Sequelize({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      models: [CategoryModel],
+    });
+    await sequelize.sync({ force: true });
+  });
+
+  afterAll(async () => {
+    await sequelize.close();
+  });
+
+  
+it("should throws error when category is invalid", () => {
+  const model = CategoryModel.build({
+    category_id: "c3b7f9f6-0b43-4f1e-a3b3-72c469af8d68",
+  })
+  try {
+    CategoryModelMapper.toEntity(model);
+    fail("Should throw an error");
+  } catch (error) {
+    expect(error).toBeInstanceOf(EntityValidationError);
+    expect((error as EntityValidationError).error).toMatchObject(
+        {
+          name: [
+            "name should not be empty", 
+            "name must be a string", 
+            "name must be shorter than or equal to 255 characters"
+          ],
+        },
+      );
+  }
+})
+  
+
+  
+
+  
+});
