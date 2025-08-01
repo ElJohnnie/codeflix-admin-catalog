47d332fb1101fa82d5bb6b9c81ecbfac8f30eab1
Jonatha Follmer <johnfollmer1995@gmail.com>
2025-08-01 11:26:40 -0300

    create and update category usecase

---
+++ src/category/application/create-category/__tests__/unit/create-category.use-case.spec.ts
@@ -0,0 +1,86 @@
+import { CategoryInMemoryRepository } from "../../../../infra/db/in-memory/category-in-memory.repository";
+import { CreateCategoryUseCase } from "../../create-category.use-case";
+
+describe("CreateCategoryUseCase Unit Tests", () => {
+  let useCase: CreateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new CreateCategoryUseCase(repository);
+  });
+
+  test("should create a category", async () => {
+    const spyInsert = jest.spyOn(repository, "insert");
+    const input = {
+      name: "Test Category",
+      description: "Test Description",
+      is_active: true,
+    };
+
+    const output = await useCase.execute(input);
+
+    expect(spyInsert).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: repository.items[0].category_id.id,
+      name: "Test Category",
+      description: "Test Description",
+      is_active: true,
+      created_at: repository.items[0].created_at,
+    });
+  });
+
+  test("should create a category with only name", async () => {
+    const input = {
+      name: "Test Category",
+    };
+
+    const output = await useCase.execute(input);
+
+    expect(output).toStrictEqual({
+      id: repository.items[0].category_id.id,
+      name: "Test Category",
+      description: null,
+      is_active: true,
+      created_at: repository.items[0].created_at,
+    });
+  });
+
+  test("should create a category with null description", async () => {
+    const input = {
+      name: "Test Category",
+      description: null as string | null,
+      is_active: false,
+    };
+
+    const output = await useCase.execute(input);
+
+    expect(output).toStrictEqual({
+      id: repository.items[0].category_id.id,
+      name: "Test Category",
+      description: null,
+      is_active: false,
+      created_at: repository.items[0].created_at,
+    });
+  });
+
+  test("should throw error when name is invalid", async () => {
+    const input = {
+      name: "",
+      description: "Test Description",
+      is_active: true,
+    };
+
+    await expect(useCase.execute(input)).rejects.toThrow("Validation Error");
+  });
+
+  test("should throw error when name is too long", async () => {
+    const input = {
+      name: "a".repeat(256), // More than 255 characters
+      description: "Test Description",
+      is_active: true,
+    };
+
+    await expect(useCase.execute(input)).rejects.toThrow("Validation Error");
+  });
+});
---
+++ src/category/application/update-category/__tests__/unit/update-category.use-case.spec.ts
@@ -0,0 +1,47 @@
+import { NotFoundError } from "../../../../../@shared/domain/errors/not-found.error";
+import { InvalidUuidError, Uuid } from "../../../../../@shared/domain/value-objects/uuid.vo";
+import { Category } from "../../../../domain/category.entity";
+import { CategoryInMemoryRepository } from "../../../../infra/db/in-memory/category-in-memory.repository";
+import { UpdateCategoryUseCase } from "../../update-gategory.use-case";
+
+describe("UpdateCategoryUseCase Unit Tests", () => {
+  let useCase: UpdateCategoryUseCase;
+  let repository: CategoryInMemoryRepository;
+
+  beforeEach(() => {
+    repository = new CategoryInMemoryRepository();
+    useCase = new UpdateCategoryUseCase(repository);
+  });
+
+  it("should throw InvalidUuidError when ID is invalid", async () => {
+    await expect(useCase.execute({ id: "fake id", name: "fake name" }))
+      .rejects.toThrow(InvalidUuidError);
+  });
+
+  it("should throw NotFoundError when entity not found", async () => {
+    const uuid = new Uuid();
+
+    await expect(useCase.execute({ id: uuid.id, name: "fake name" }))
+      .rejects.toThrow(NotFoundError);
+  });
+
+  it("should update a category", async () => {
+    const spyUpdate = jest.spyOn(repository, "update");
+    const entity = new Category({ name: "Movie"});
+
+    repository.items = [entity]
+
+    let output = await useCase.execute({
+      id: entity.category_id.id,
+      name: "Updated Name",
+    })
+    expect(spyUpdate).toHaveBeenCalledTimes(1);
+    expect(output).toStrictEqual({
+      id: entity.category_id.id,
+      name: "Updated Name",
+      description: null,
+      is_active: true,
+      created_at: entity.created_at, 
+    })
+  });
+});
